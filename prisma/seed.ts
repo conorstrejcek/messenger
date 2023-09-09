77ac8942fea6a4a877214a9dcaa87de77e726597
Conor Strejcek <cstrejcek@plaidypus.com>
2023-09-08 22:42:51 -0500

    feat: Add prisma with sqlite

---
+++ prisma/seed.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const user1 = await prisma.user.create({
+    data: {
+      name: "Martin Cooper",
+    },
+  });
+
+  const user2 = await prisma.user.create({
+    data: {
+      name: "Joel Engel",
+    },
+  });
+
+  console.log({ user1, user2 });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
