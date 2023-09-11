59306db3fa41e8778fa0ef7ae65628f7591145e9
Conor Strejcek <cstrejcek@plaidypus.com>
2023-09-10 20:36:04 -0500

    feat: Add ability to send with return key

--- components/user/MessageInput.tsx
+++ components/user/MessageInput.tsx
@@ -12,6 +12,15 @@ export function MessageInput(props: { senderId: string }) {
   const [sendMessageMutation] = useMutation(SEND_MESSAGE);
   const [text, setText] = useState("");
 
+  const handleKeyDown: React.KeyboardEventHandler<HTMLTextAreaElement> = (
+    event,
+  ) => {
+    if (event.key === "Enter" && !event.shiftKey) {
+      event.preventDefault(); // Prevent adding a new line
+      sendMessage();
+    }
+  };
+
   const sendMessage = async () => {
     if (!text.trim()) {
       return;
@@ -31,19 +40,27 @@ export function MessageInput(props: { senderId: string }) {
 
   return (
     <div className="mt-4">
-      <div className="left-0 right-0 p-4 bg-white shadow-lg flex items-center mx-auto">
-        <textarea
-          className="flex-grow border p-2 rounded mr-2 text-gray-800"
-          placeholder="Type your message..."
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
-        <button
-          onClick={sendMessage}
-          className="bg-blue-500 text-white p-2 rounded"
-        >
-          Send
-        </button>
+      <div className="p-4 bg-white shadow-lg items-center">
+        <div className="flex">
+          <div className="flex-grow mr-2 relative">
+            <textarea
+              className="w-full border p-2 rounded text-gray-800"
+              placeholder="Type your message..."
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+              onKeyDown={handleKeyDown}
+            />
+            <div className="text-right text-xs text-gray-500 absolute right-0 bottom-0 -mb-3">
+              Shift + Return to add a new line
+            </div>
+          </div>
+          <button
+            onClick={sendMessage}
+            className="bg-blue-500 text-white p-2 rounded"
+          >
+            Send
+          </button>
+        </div>
       </div>
     </div>
   );
